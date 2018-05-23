4ff42bc9c2fdbc415c2e2d0e5f4e36bb9ca94bcb
kwoningi <kuz312@naver.com>
2018-05-23 11:38:11 +0900

    1

--- R/ft.R
+++ R/ft.R
@@ -1,30 +1,28 @@
+
+library(ggplot2)
+
+# Set arbitrary time & Gravitational acceleration
 t <- seq(0, 100, 0.1)
 g <- 9.8
-library(ggplot2)
+
 player <- function(a, b) {
   rdelement <<- runif(1, -10, 10)
-  if (a > 0) {
+  if (a > 0 & b > 0 & b < pi/2){
     v <<- a + runif(1, -10, 10 + rdelement)
-  } else {
-     print("The Speed must be greater than zero!")
-  }
-
-  if (b > 0 & b < pi/2) {
     theta <<- b + runif(1, -0.1, 0.1)
+    x <- v*cos(theta)*t
+    y <- v*sin(theta)*t - 0.5*g*t^2
+    x <- x[y >= 0]
+    y <- y[y >= 0]
+
+    df <- data.frame(x, y)
+    ggplot(data = df,aes(x = x, y = y)) + coord_cartesian(xlim=c(0,5000), ylim=c(0,1500)) +
+      geom_line(linetype = 5, size = 1, colour = "blue")
+
   } else {
-    print("The angle must be greater than zero and smaller than 1.57 radian!")
+    print("The Speed must be greater than zero and The angle must be greater than zero and smaller than 1.57 radina!")
   }
-
-  x <- vcos(theta)t
-  y <- vsin(theta)t - 0.5*gt^2
-
-  x <- x[y >= 0]
-  y <- y[y >= 0]
-
-  df <- data.frame(x, y)
-  ggplot(data = df, aes(x = x, y = y)) +
-    geom_line(linetype = 5, size = 1, colour = "blue")
 
 }
 
-player(100, 2)
+player(200, 1)
